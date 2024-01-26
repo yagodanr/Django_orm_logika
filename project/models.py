632b003d5edd49f4476a42ce88b9e22ab914963c
yagodanr <yagodanr@gmail.com>
2024-01-26 23:29:33 +0200

    Game and Genre

--- project/models.py
+++ project/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Genre(models.Model):
+    name = models.CharField(max_length=100)
+    
+    
+class Game(models.Model):
+    name = models.CharField(max_length=100)
+    genres = models.ManyToManyField(Genre)
+    release_date = models.DateField()
+    rating = models.DecimalField(max_digits=3, decimal_places=2)
